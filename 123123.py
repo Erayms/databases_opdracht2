c88aa6275f22a84ce3b7cbaae6c9f6a80cf8d0ee
Mashu02 <94843782+Mashu02@users.noreply.github.com>
2022-03-11 15:23:46 +0100

    Add files via upload
    functie bestaan maar csv file laden moet nog

--- 123123.py
+++ 123123.py
@@ -1,29 +1,30 @@
 from pymongo import *
-import pprint
 import pandas as pd
-from random import randint
 
 client = MongoClient()
 db = client["huwebshop"]
 
-products_col = db["products"]
-profiles_col = db["profiles"]
-sessions_col = db["sessions"]
+
+def products():
+    products_col = db["products"]
+    products = [x for x in products_col.find()]
+    col = ['_id', 'category', 'price', 'gender', 'brand', 'sub_category']
+    df = pd.DataFrame(products, columns=col)
+    df.to_csv("products", index=False)
 
 
-products = [x for x in products_col.find()]
-profiles = [x for x in profiles_col.find()]
-sessions = [x for x in sessions_col.find()]
+def profiles():
+    profiles_col = db["profiles"]
+    profiles = [x for x in profiles_col.find()]
+    col2 = ['_id', 'recommendations', 'previously_recommended']
+    df = pd.DataFrame(profiles, columns=col2)
+    df.to_csv("profiles", index=False)
 
-col = ['_id', 'category', 'price', 'gender', 'brand', 'sub_category']
-df = pd.DataFrame(products, columns=col)
-df.to_csv("products", index=False)
+def sessions():
+    sessions_col = db["sessions"]
+    sessions = [x for x in sessions_col.find()]
+    col3 = ['_id', 'user_agent']
+    df = pd.DataFrame(sessions, columns=col3)
+    df.to_csv("sessions", index=False)
 
-col2 = ['_id', 'recommendations', 'previously_recommended']
-df = pd.DataFrame(profiles, columns=col)
-df.to_csv("profiles", index=False)
-
-col3 = ['_id', 'recommendations', 'previously_recommended']
-df = pd.DataFrame(profiles, columns=col)
-df.to_csv("profiles", index=False)
-
+sessions()
