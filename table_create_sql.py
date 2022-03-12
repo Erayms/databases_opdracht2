8a5bcc459699c1e680165322753c1ef2773d7916
Mashu02 <94843782+Mashu02@users.noreply.github.com>
2022-03-12 18:09:15 +0100

    Add files via upload
    werkt nog niet maar is voor de table creation in de postgresql

---
+++ table_create_sql.py
@@ -0,0 +1,45 @@
+import psycopg2
+
+# vul je eigen gegevens in van de sql database, !verander alleen de database en wachtwoord!
+con = psycopg2.connect(
+        host='localhost',
+        database='huwebshop',
+        user='postgres',
+        password='MattyJim02')
+cursor = con.cursor()
+
+# de table met infomatie en datatype
+cursor.execute("DROP TABLE IF EXISTS products")
+cursor.execute("DROP TABLE IF EXISTS profiles")
+cursor.execute("DROP TABLE IF EXISTS sessions")
+
+info = (
+   '''
+CREATE TABLE products(
+   _id BIGSERIAL NOT NULL, 
+   naam varchar(100),
+   price int,
+   brand varchar(100),
+   category varchar(100),
+   sub_category varchar(100),
+   gender varchar(100)
+   )
+''',
+'''
+CREATE TABLE profiles(
+   _id BIGSERIAL NOT NULL, 
+   recommendations varchar(100),
+   previously_recommended varchar(100)
+   )
+''',
+'''
+CREATE TABLE sessions(
+   _id BIGSERIAL NOT NULL, 
+   buid BIGSERIAL NOT NULL,
+   preferences varchar(100))                     
+''')
+
+cursor.execute(info)
+con.commit()
+con.close()
+
